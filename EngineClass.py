f8cf5f196765b828471da72fb72c3c6bb54cda0c
Xharmander404 <115119143+Xharmander404@users.noreply.github.com>
2025-05-11 22:17:16 -0700

    Initial frame to get started. We should talk about this during engine.

---
+++ EngineClass.py
@@ -0,0 +1,60 @@
+import numpy as np
+from functools import cached_property
+
+#Everything is in SI
+# Here are a bunch of functions pasted from a diff calculator.
+def C_star(Chamber_press, m_dot, A_throat):
+    C = (Chamber_press * A_throat)/m_dot
+    return C
+
+#C* is a performance metric called characteristic velocity
+
+def A_star(m_dot, P_0, T_0, R, gamma):
+    A = (m_dot/P_0) * (np.sqrt((T_0 * R)/gamma)) * ((1+((gamma-1)/2))**((gamma+1)/(2*(gamma-1))))
+    return A
+
+def chamber_vol(L_star, A_star):
+    V_ch = L_star * A_star
+    return V_ch
+
+def F_thrust(m_dot, V_e, A_e, P_e, P_atm):
+    thrust = (m_dot * V_e) + A_e * (P_e - P_atm)
+    return thrust
+
+def m_dot(thrust, V_e, A_e, P_e, P_atm):
+    m_dot = (thrust - (A_e * (P_e - P_atm))) / (V_e)
+    return m_dot
+
+def exit_velo(T_0, M_bar, R_bar, gamma, P_e, P_0):
+    tempOne = ((2*R_bar*gamma*T_0)/((gamma - 1)*M_bar))
+    tempTwo = (1 - ((P_e/P_0) ** ((gamma-1)/gamma)))
+    V_e = np.sqrt(tempOne * tempTwo)
+    return V_e
+
+def incompres_Area(m_dot, cD, rho, delta_P):
+    A = m_dot / (cD * np.sqrt(2 * rho * delta_P))
+    return A
+
+def IstrpcTemp(T_0, gamma, Mach):
+    T = T_0 * ((1 + (((gamma - 1)/2) * Mach**2) ) ** (-1))
+    return T
+
+
+
+#Define the class here.
+class engine():
+    def __init__(self, OF, Pressure_pa, Thrust_N):
+        self.OF = OF
+        self.Pressure = Pressure_pa
+        self.Thrust = Thrust_N
+        pass
+    #Run Cea and define the rest of the engine if accessed? Still not sure whether to calculatue it on startup? Let's see if Rocketcea is quick.
+    @cached_property
+    def CEA(self):
+        print("Running CEA...")
+        #Calculate everything
+        return 
+
+
+#So from what I understand, we should run CEA on initializations always but things like thermals or other potentially intensive tasks we should run in a cached property. 
+#I guess we create the plotting function here too ig??? Also presumably the propellant feed calc should be in a seperate python file just for cleanliness? 
